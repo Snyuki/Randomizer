cdfe8dd30ade62c5061f453dea34cd3ef50e79de
CuV-Dev <58642374+CuV-Dev@users.noreply.github.com>
2022-09-23 18:06:42 +0200

    Added Comments to the Script

--- Code/src/randomizer.py
+++ Code/src/randomizer.py
@@ -1,12 +1,14 @@
-from mimetypes import init
 import random
 from time import sleep
 from tkinter import *
 from tkinter import ttk
-from turtle import title, width, window_height, window_width
 
 
 class RandomizeGUI():
+    """
+    RandomizeGUI initializes a Graphical User Interface (GUI) in which a
+    User can let the Program select a random Element of a List of Choices.
+    """
 
     def __init__(self):
         # Set some Variables
@@ -31,6 +33,10 @@ class RandomizeGUI():
         
         
     def setRootConfigurations(self):
+        """
+        setRootConfigurations sets the initial values for the root Frame of the GUI
+        including the initial position of the Frame.
+        """
         root = self.root
         root.title("Randomizer")   # Set Title
         root.resizable(False, False)   # Set Non-Resizeable
@@ -47,16 +53,30 @@ class RandomizeGUI():
         
         
     def randomize(self):
+        """
+        randomize Selects a Random element of the list of choices.
+
+        Returns:
+            str: The choice
+        """
         return random.choice(self.choices)
 
 
     def updateRandomizer(self):
+        """
+        updateRandomizer Calls the randomize function of this class and updates
+        the Label of the GUI with the returned choice.
+        """
         randomChoice = self.randomize()
         self.displayChoiceLabel.config(text=randomChoice)
         self.displayChoiceLabel.update()
         
         
     def multiRandomize(self):
+        """
+        multiRandomize Calls the updateRandomizer function of this class multiple
+        times in a progressing time period.
+        """
         self.randomizeButton["state"] = "disabled"
         waitTime = 0.01
         
@@ -69,6 +89,9 @@ class RandomizeGUI():
         
         
     def start(self):
+        """
+        start Starts the mainloop of the Frame
+        """
         self.root.mainloop()
         
 
